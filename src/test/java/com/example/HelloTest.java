93d06c49dfea39e70fe21714aab382aac3246c1f
kuwa <t.kuwazaki1024@gmail.com>
2025-08-01 16:16:28 +0000

    テストコード追加

---
+++ src/test/java/com/example/HelloTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import com.example.model.Greeting;
+import com.example.model.User;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloTest {
+
+    @Test
+    public void test() {
+        Mono<Greeting> result = new Hello().apply(Mono.just(new User("foo")));
+        assertThat(result.block().getMessage()).isEqualTo("Hello, foo!\n");
+    }
+}
